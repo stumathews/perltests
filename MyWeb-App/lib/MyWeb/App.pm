0ff21ebcc11c54692df5ca7dbcf719e1f1addde0
Stuart Mathews <Stuart.Mathews@citrix.com>
2017-08-26 16:11:53 +0100

    randome addition

--- MyWeb-App/lib/MyWeb/App.pm
+++ MyWeb-App/lib/MyWeb/App.pm
@@ -7,65 +7,44 @@ get '/' => sub {
     template 'index.tt',{ 'title' => "Stuart's Cool website" };
 };
 
-get '/simpleaddition' => sub {
-	my %addition = (
-		"1+2"=>3, "2+3"=>5, "3+4"=>7, "4+5"=>9, "5+6"=>11, "6+7"=>13, "7+8"=>15,"8+9"=>17		
-		);
-    template 'simpleaddition.tt', { 
-							'title' => "Simple addition",
+get '/consecutiveaddition1' => sub {
+	my %addition = ("1+2"=>3, "2+3"=>5, "3+4"=>7, "4+5"=>9, "5+6"=>11, "6+7"=>13, "7+8"=>15,"8+9"=>17, "9+10"=>19);
+    template 'addition.tt', { 
+							'title' => "Consecutive addition level 1",
 							'sums' => \%addition,
 	};
 };
 
-get '/randomaddition' => sub {
-	my %randomaddition = (
-		"1+2"=>3,
-		"2+3"=>5,
-		"3+4"=>7,
-		"4+5"=>9,
-		"5+6"=>11,
-		"6+7"=>13,
-		"7+8"=>15,
-		"8+9"=>17,
-		"9+10"=>19,
-		"10+11"=>21,		
-		"11+12"=>23,
-		"12+13"=>27,
-		"13+14"=>27,
-		"14+15"=>29,
-		"15+16"=>31,
-		"16+17"=>33,
-		"17+18"=>35,
-		"18+19"=>37,
-		"19+20"=>39,					
-		"8+9"=>17
-		);
-    template 'simpleaddition.tt', { 
-							'title' => "Random addition",
+get '/consecutiveaddition2' => sub {
+	my %randomaddition = ("1+2"=>3, "2+3"=>5,"3+4"=>7,"4+5"=>9,"5+6"=>11,"6+7"=>13,"7+8"=>15,"8+9"=>17,"9+10"=>19,
+		"10+11"=>21,"11+12"=>23,"12+13"=>27,"13+14"=>27,"14+15"=>29,"15+16"=>31,"16+17"=>33,"17+18"=>35,"18+19"=>37,
+		"19+20"=>39,"8+9"=>17 );
+    template 'addition.tt', { 
+							'title' => "Consecutive addition level 2",
 							'sums' => \%randomaddition,
 	};
 };
 
 get '/hardaddition' => sub {
-	my %randomaddition = (
-		"6+3"=>9,
-		"7+2"=>9,
-		"4+7"=>11,
-		"9+2"=>11,
-		"8+3"=>11,
-		"7+5"=>12,
-		"9+3"=>12,
-		"8+4"=>12,
-		"9+4"=>13,
-		"8+5"=>13,		
-		"8+6"=>14,
-		"9+5"=>14,
-		"9+7"=>16,
-		"9+6"=>15		
-		);
-    template 'simpleaddition.tt', { 
+	my %randomaddition = ( "6+3"=>9, "7+2"=>9, "4+7"=>11,"9+2"=>11, "8+3"=>11, "7+5"=>12, "9+3"=>12,"8+4"=>12, "9+4"=>13,
+		"8+5"=>13, "8+6"=>14,"9+5"=>14, "9+7"=>16, "9+6"=>15);
+    template 'addition.tt', { 
 							'title' => "Random addition",
 							'sums' => \%randomaddition,
+	};
+};
+
+get '/randomaddition/:upto' => sub {
+	my %sums = ();
+	foreach my $index (1..int(route_parameters->get('upto'))){
+		my $term1 = int(rand(100));
+		my $term2 = int(rand(100));
+		my $string = $term1."+".$term2;
+		$sums{$string} = $term1+$term2;
+	}
+    template 'addition.tt', { 
+							'title' => "Random addition",
+							'sums' => \%sums,
 	};
 };
 

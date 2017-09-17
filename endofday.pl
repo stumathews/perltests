74d34f61b646e0f0881d25fbd5a9dc5c68d6de2d
Stuart Mathews <stumathews@gmail.com>
2017-09-17 19:42:44 +0100

    tempo commit

--- endofday.pl
+++ endofday.pl
@@ -10,13 +10,22 @@ use Data::Dumper;
 use URI::Encode qw(uri_encode uri_decode);
 use Getopt::Std;
 
+
 #Global store of all tickers and their stock details
-my %all;
+my %all = ();
+
+# the actuual cache of tickers/names
+my %company_tickers;
+
 my %options=();
-getopts("ht:d:o:r:l:vx:bpgc", \%options);
+getopts("ht:d:o:r:l:vx:bpgcsmi", \%options);
 
 my $verbose = $options{v};
 my $colimit = $options{l};
+my $cacheFileName = "co2tick.cache";
+my $haveTickerCache = -e $cacheFileName;
+my $printCache = $options{p};
+my $numThreads = $options{t} || 2; 
 
 # print arguments used if verbosed mode used
 if($verbose) {
@@ -44,7 +53,10 @@ Options:
  -p : show name to ticker cache (used to prevent re-lookup of ticker symbols for known companies)
  -g : show saved proGress
  -c : print all Companies.
- 
+ -s : skip company to ticker resolution - use only known tickers
+ -e : introduce random sleep to confuse spam detectors
+ -m : print cache misses
+ -i - print cache hits
  Examples:  perl endofpday.pl -v companies.csv
             perl endofday.pl -b -x excludefile.csv companies.csv
             perl endofday.pl -d ',' -o 'myoutputfile.csv' companies.csv
@@ -71,121 +83,123 @@ sub decode_name {
 }
 
 # Looks up the company name on the internet to get its corresponding ticker symbol (which is used later to get its stock details)
-sub ConvertCompanyToTicker {
-	my @args = @_;
-	my $company = uri_encode(shift @args);
+sub ConvertEncodedCompanyToTicker {    
+	my @args = @_;		
+	my $encoded_company = shift @args;	
+	my $decoded_company = decode_name($encoded_company);
+	my $company_uri =  uri_encode($decoded_company);	
 	my $region = shift @args || ($options{r} || "us");
 	my $lang = shift @args ||  ($options{l} || "en-gb");
-	my $json = getJson("http://d.yimg.com/aq/autoc?query=$company&region=$region&lang=$lang");
+	print "LIVE ConvertEncodedCompanyToTicker : $decoded_company\n";
+	my $json = getJson("http://d.yimg.com/aq/autoc?query=$company_uri&region=$region&lang=$lang");	
 	if ($json) {
 	    my $jObj = decode_json($json);
-	    my @queryResult = @{$jObj->{'ResultSet'}{'Result'}};
-	    for my $var (@queryResult) { 
-		    return $var->{symbol};
+	    my @queryResult = @{$jObj->{'ResultSet'}{'Result'}};	    
+	    for my $var (@queryResult) {                        
+            my $ticker = $var->{symbol};            
+            print "company = '$decoded_company' ticker --> $ticker\n";
+		    return $ticker;
 	    }
 	}
 	return undef;
 }
 
-my $count = 0;
 # Look up the tickers stock details on the internet
 sub ConvertTickerToStock {
+
 	my @args = @_;
 	my $ticker = shift @args;
 	my $url = "https://query.yahooapis.com/v1/public/yql?q=".
 		           "select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" .$ticker.
-			   "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
-	my $total = scalar(keys %all);
-	my $end_run = time();
-	my $start_run = $^T;
-	my $run_time = $end_run - $start_run;
-	print "Fetching: [$ticker] ",++$count,"/", $total, "(",sprintf("%1d",($count/$total)*100),"%) Elapsed: $run_time secs. ";
+			   "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";	
+	#print $url,"\n" if $verbose;	
 	my $json = getJson($url);
 	if($json) {
 		my $jObj = decode_json($json);
-		my $queryResult = $jObj->{'query'}{'results'}{'quote'};
-		# Keep track of our progress so far
-		$all{$ticker} = $queryResult || undef;
-		# print Huge output?
-		print Dumper($queryResult) if $verbose;
-		return $all{$ticker};
-	}
+		my $queryResult = $jObj->{'query'}{'results'}{'quote'};		
+		my $Name = $queryResult->{'Name'} || "none" ;
+        my $Currency = $queryResult->{'Currency'} || "none" ; 
+        my $Ask = $queryResult->{'Ask'} || "none" ; 
+        my $Open = $queryResult->{'Open'} || "none" ; 
+        my $PreviousClose = $queryResult->{'PreviousClose'} || "none" ; 
+        my $PercentChange = $queryResult->{'PercentChange'} || "none" ;
+        my $PriceBook = $queryResult->{'PriceBook'} || "none" ; 
+        my $Change = $queryResult->{'Change'} || "none" ; 
+        my $DaysHigh  = $queryResult->{'DaysHigh'} || "none" ;
+        my $DaysLow = $queryResult->{'DaysLow'} || "none" ;
+        my $EarningsShare = $queryResult->{'EarningsShare'} || "none" ;
+		print sprintf("%36s %s %s %s %s %s %s %s %s %s %s",$Name,$Currency,$Ask,$Open,$PreviousClose,$PercentChange,$PriceBook, $Change,$DaysHigh,$DaysLow,$EarningsShare);
+		#print Dumper($queryResult) if $verbose;
+		return $queryResult
+	}  
 	return undef;
 }
 
-my $total_payload = 0;
+
 # Send actual request as HTTP/S
 sub getJson {
 	my @args = @_;
-	my $url = shift @args;
+	my $url = shift @args;	
 	my $req = HTTP::Request->new(GET => $url);
        $req->header('content-type' => 'application/json');
 	my $ua = LWP::UserAgent->new;
-        my $resp = $ua->request($req);
-		my $headers =  $resp->headers();
-		my $content_size = length($resp->content);
-		$total_payload+=$content_size;
-		print "Recieved: ",sprintf("%1d",$content_size/1024)," Kb Recieve Total:", sprintf("%1d",($total_payload/1024)/1024), " Mb (",sprintf("%1d",$total_payload/1024),"Kb)\n";
-
-        if ($resp->is_success) {
-	    return $resp->decoded_content;
-        } else {
-            print "HTTP GET error code: ", $resp->code, "\n";
-            print "HTTP GET error message: ", $resp->message, "\n";
-	    return undef;
-        }
+	#introduce random sleep.
+    sleep rand($options{t}) if $options{e};
+    my $resp = $ua->request($req);        
+    if ($resp->is_success) { 
+        return $resp->decoded_content;
+    } else {
+        print "HTTP GET error code: ", $resp->code, "\n";
+        print "HTTP GET error message: ", $resp->message, "\n";
+        return undef;
+    }
 }
 
-# name of cache of already resolved ticker names from company names
-my $cacheFileName = "co2tick.cache";
-# name of progress cache 
-my $progressCacheFileName = "progress.cache";
 
-my $haveTickerCache = -e $cacheFileName;
-my $haveProgressCache = -e $progressCacheFileName;
-# the actuual cache of tickers/names
-my %resolutionCache;
-
-my $printCache = $options{p};
 if($haveTickerCache) {
 	#Read in existing file-Cache of ticker to company names to prevent relookup(expensive)
-	%resolutionCache = %{ retrieve($cacheFileName) };
+	%company_tickers = %{ retrieve($cacheFileName) };
 	# print the company vs ticker cache is asked
 	if($printCache) {
 		print "Printing company vs ticker cache...\n";
 		my $count = 0;
-		while ((my $company, my $ticker) = each(%resolutionCache)) {
+		my %hits = ();
+		my %misses = ();
+
+		while ((my $company, my $ticker) = each(%company_tickers)) {
 			$company = decode_name($company);
-			print "cache entry #",$count++, ": Company: $company Ticker --> ",$ticker,"\n";
+            if($ticker){
+                $hits{$company} = $ticker;
+                print "cache entry #",$count++, ": Company: $company Ticker --> ",$ticker,"\n";
+            } else {
+                $misses{$company} = undef;
+                print "cache entry #",$count++, ": Company: $company Ticker --> MISS","\n";
+            }
 		}
+		
+		if($options{m})
+		{
+            print "Cache misses:\n";
+            print map { "miss $_\n"} sort keys %misses;
+		}
+		if($options{i})
+		{
+            print "Cache hits:\n";
+            print map { "hit $_\n"} sort keys %hits;
+		}
+		print "total hits: ",scalar(keys %hits), ", total misses: ",scalar(keys %misses),"\n";
 		exit 0;
 	}
 }
-
-my $printProgress = $options{g};
-if($haveProgressCache) {
-	#Read in any progress we've already made before we start processing
-	%all = %{ retrieve($progressCacheFileName) };
-	#print the progress cache if asked
-	if($printProgress) {
-		print "Printing progress cache...\n";
-		my $count = 0;
-		print Dumper(\%all);		
-		exit 0;
-	}
-}
-
-# Ctrl+C event handler will save any progress if interrupted mid-flow...
-$SIG{'INT'} = sub {
-	print "Abort!\n";
-	print "Saving progress...";	
-	store(\%all, $progressCacheFileName);
-	print "Done.\n";	
-	exit 1;	
-};
 
 # read in all the companies 
 my @companies = <>;
+
+#remove linefeed and carrige returns
+s/\r|\n//g for @companies;
+
+# We like companies without spaces so encode them
+@companies = map { encode_name($_)} @companies;
 
 #print companies if asked to, -c
 if($options{c}){
@@ -210,70 +224,32 @@ if($excludeFile && -e $excludeFile) {
 	close EXCLUDE or die "Cannot close $excludeFile: $!"; 
 }
 
+my %defined = ();
+my %undefined = ();
+while( my( $company, $ticker ) = each %company_tickers ){
+    $defined{$company} = $ticker if $ticker; #cache hits
+    $undefined{$company} = $ticker if !$ticker; #cache misses
+}
 
-## 
-## Main work begins from this point forward.
-## 
-
-
-# process companies as to convert them to tickers, saving any resolved tickers to cache...
-my $lineCount = 0;
-foreach my $line(@companies) {
-	my $company = $line;
-	chomp $company; #forget newlines on the ends of company names
-	next if !$company;
-	my $ticker;
-
-	# We're going to store company names with spaces to underscores so we can have one-worded companies in the cache for easier comparisons and alter lookups
-	$company = encode_name($company);
-	# Look up ticker for company in the cache first...
-	$ticker = $resolutionCache{$company};
-
-	# got a ticker?
-	if(!$ticker) { 
-		# Nope, so lets try and fetch one oneline for this company...
-		$company = decode_name($company);
-		print "LIVE convertCompanyToTicker Company:'$company'\n";
-		$ticker = ConvertCompanyToTicker($company);
-		print ($ticker || "Could not resolved to a ticker symbol.");
-		
-		my $addbadTickersToExcludeFile = $options{b};
-		if($addbadTickersToExcludeFile && $excludeFile && !$ticker) {
-			print "Adding bad company, '$company' to exclude file\n";
-			open(my $ex, '>>', $excludeFile) or die "Could not open file '$excludeFile' $!";
-			print $ex "$company\n";
-			close $ex;
-		}
-		print "\n";		
-		next if(!$ticker);
-	}else{
-		print "CACHE HIT convertCompanyToTicker Company:'$company'\n";
-	}
-	
-	
-	# Make a space for this ticker's results...undef means no stock results for this ticker yet
-	$all{$ticker} = undef;
-
-	# Put the multiword company name in the hash that tracks company vs ticker names cache
-	$company = encode_name($company);
-	$resolutionCache{$company} = $ticker;
-
-	# if asked to, stop process after specified set amount of company have been processed.
-	last if ($colimit && ($lineCount++ == $colimit));
+print "sending in only ", scalar(keys %undefined), " companies for resolution\n";
+print scalar(keys %defined), " have already been found via cache\n";
+print "both combined = ", scalar(keys %company_tickers),"\n";
+if(!$options{s}){
+    #Pass one: Get all known company tickers and update the company_tickers value as ticker symbol
+    my %output1 = iterate_as_hash({ workers => $numThreads },\&ConvertEncodedCompanyToTicker, \%undefined);
+    %company_tickers = (%defined, %output1);
 }
 
 print "Finished Pass one : Resolving company names to tickers\n";
 print "Saving results in $cacheFileName\n\n";
-store(\%resolutionCache, $cacheFileName);
-
-# now start the fetching of ticker stock data:
-my $numThreads = $options{t} || 2; 
-my @columns;
-
-# Process the tickers as to convert them to stock details.
-# This is currenly entirly handled by Paralell::Iterator's iterate_as_hash function.
+store(\%company_tickers, $cacheFileName);
+    
+while( my($company, $ticker) = each %company_tickers ){        
+    $all{$ticker} = undef if $ticker;    
+}
+#Pass 2: Process the tickers as to convert them to stock details.
 my %output = iterate_as_hash({ workers => $numThreads },\&ConvertTickerToStock, \%all);
-%all = (%all, %output);
+%all = (%defined, %output);
 
 print "Phase 2 Complete: Fetching stock data for tickers\n";
 print "Writing all stock data to CSV...\n";
@@ -284,6 +260,7 @@ my $timeStamp = sprintf("%04d-%02d-%02d_%02d-%02d-%02d",
                         $now[5]+1900, $now[4]+1, $now[3],
                         $now[2],      $now[1],   $now[0]);
 
+my @columns;
 # Write to output file
 open(my $csv, '>', $options{o} || "stocks_$timeStamp.csv");
 foreach my $ticker (sort keys %all) {
@@ -314,13 +291,9 @@ foreach my $ticker (sort keys %all) {
 	@line = undef;
 }
 close($csv);	
-# We've finished and dont need a progress file anymore. Delete it
-print "Cleaning up...\n";
-unlink $progressCacheFileName;
+
 
 my $end_run = time();
 my $start_run = $^T;
 my $run_time = $end_run - $start_run;
 print "Job took $run_time seconds\n";
-
-
